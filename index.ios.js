d201b2c36763b4576763b6d7ffa2747717561713
Jeremy Danner <jeremydanner2@gmail.com>
2017-10-16 20:41:32 +0200

    fixed redux

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
 import App from './src';
 
-AppRegistry.registerComponent('idrottskollApp', () => App);
+const store = configureStore();
+
+const ReduxApp = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent('idrottskollApp', () => ReduxApp);
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+// https://reactnavigation.org/docs/navigators/tab
+// http://rants.broonix.ca/getting-started-with-react-native-and-redux/
+// https://www.udemy.com/react-redux-tutorial/learn/v4/t/lecture/4962610?start=0
--- src/Views/Profile/Profile.js
+++ src/Views/Profile/Profile.js
@@ -3,49 +3,18 @@ import { ScrollView, View, TextInput, StyleSheet, Button } from 'react-native';
 import * as actions from '../../actions';
 import MainStyles from '../../assets/styles/MainStyles';
 import Header from '../../components/Header/Header';
-import { reduxForm } from 'redux-form';
 
 export default class Profile extends React.Component {
     constructor(props) {
         super(props);
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        // this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
-
-    handleFormSubmit({ email, password }) {
-        //verify email and password
-        this.props.signinUser({ email, password });
-    }
-
     render() {
         const { navigate } = this.props.navigation;
-        const { handleSubmit, fields: { email, password } } = this.props;
         return (
             <View style={MainStyles.VIEW_CONTAINER}>
                 <Header />
-                <ScrollView>
-                    <View style={[MainStyles.MAIN_CARD]}>
-                        <TextInput
-                            style={[styles.input, styles.email]}
-                            autoCorrect={false}
-                            keyboardType="email-address"
-                            placeholder="E-post Adress"
-                            returnKeyType="next"
-                        />
-
-                        <TextInput
-                            style={[styles.input, styles.password]}
-                            secureTextEntry={true}
-                            autoCorrect={false}
-                            placeholder="Lösenord"
-                            returnKeyType="send"
-                        />
-                    </View>
-                    <Button
-                        onPress={this.handleFormSubmit}
-                        title="Logga in"
-                        color="#841584"
-                    />
-                </ScrollView>
+                <ScrollView />
             </View>
         );
     }

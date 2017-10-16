830cb7d73c61723d5f17f2d9d2669d040826b00a
Jeremy Danner <jeremydanner2@gmail.com>
2017-10-16 20:40:21 +0200

    added login card

--- src/components/Cards/LoginCard.js
+++ src/components/Cards/LoginCard.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
+import {View} from 'react-native';
 import MainStyles from '../../assets/styles/MainStyles';
 
+
 const LoginCard = props => {
-    return (
+    render() {
+        return (
 
-    );
-};
-
-const styles = StyleSheet.create({
-    input: {},
-
-    email: {},
-
-    password: {},
-});
+        )
+    }
+}
 
 export default LoginCard;

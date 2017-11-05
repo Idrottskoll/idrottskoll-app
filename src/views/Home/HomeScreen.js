0b3e42db5173137ac71b22c253cccabe26d1a37a
Jeremy Danenr <jeremydanner2@gmail.com>
2017-11-05 18:15:35 +0100

    components renders from the homescreen arn now from the api

--- src/views/Home/HomeScreen.js
+++ src/views/Home/HomeScreen.js
@@ -6,7 +6,8 @@ import {
     TouchableHighlight,
     View,
     Image,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 import DefaultCard from '../../components/Cards/DefaultCard';
@@ -16,64 +17,105 @@ import ScrollViewContainer from '../../components/ScrollViewContainer';
 import MainStyles from '../../assets/styles/MainStyles';
 import LiveNowCard from '../../components/Cards/LiveNowCard';
 import StyleRules from '../../assets/styles/StyleRules';
+import { connect } from 'react-redux';
 
-export default class HomeScreen extends React.Component {
+class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderLiveSports = () => {
+        const { navigate } = this.props.navigation;
+        const live = true;
+        if (live) {
+            return (
+                <TouchableOpacity
+                    onPress={() =>
+                        navigate('VideoScreen', {
+                            videoTitle: 'innebany',
+                            videoName: 'innebany, vallhalla bana 7.',
+                            videoDescription: 'Live',
+                            isRecorded: false,
+                            uploaded: false,
+                            vidioUrl:
+                                'idrottskoll_vallhalla_bana7_datum20171031_start2008_slut2008.mp4'
+                        })}
+                >
+                    <LiveNowCard videoName="SM final innebany" />
+                </TouchableOpacity>
+            );
+        }
+    };
+
+    /**
+    * Method dose logic for sorting if a user has videos to display
+    */
+    renderComponents = () => {
+        const { navigate } = this.props.navigation;
+        if (this.props.authenticated) {
+            return (
+                <View>
+                    {this.props.data && this.props.data.video.length !== 0 ? (
+                        this.props.data.video.map(video => (
+                            <View>
+                                <View style={[MainStyles.VIDEO_BUTTON]}>
+                                    <Image
+                                        style={MainStyles.VIDEO_CONTAINER}
+                                        source={require('../../assets/icons/football.png')}
+                                    />
+                                    <TouchableHighlight
+                                        key={video._id}
+                                        underlayColor={
+                                            StyleRules.BLUE_GRADIENT_COLOR
+                                        }
+                                        activeOpacity={0.8}
+                                        style={styles.OPEN_VIDEO_BUTTON}
+                                        onPress={() =>
+                                            navigate('VideoScreen', {
+                                                videoTitle: video.sport,
+                                                videoName: `${video.sport}, ${video.club} bana ${video.court}.`,
+                                                videoDescription: `Inspelat: ${video.startTime}.`,
+                                                isRecorded: video.isRecorded,
+                                                uploaded: video.uploaded,
+                                                vidioUrl: video.name
+                                            })}
+                                    >
+                                        <Text style={styles.BUTTON_TITLE}>
+                                            Lås upp video
+                                        </Text>
+                                    </TouchableHighlight>
+                                </View>
+
+                                <View style={[MainStyles.MAIN_CARD]}>
+                                    <Text style={MainStyles.MAIN_CARD_TITLE}>
+                                        {`${video.sport}, ${video.club} bana ${video.court}.`}
+                                    </Text>
+                                    <Text>
+                                        {`Inspelat: ${video.startTime}.`}
+                                    </Text>
+                                </View>
+                            </View>
+                        ))
+                    ) : (
+                        <Text>Du har inga videos</Text>
+                    )}
+                </View>
+            );
+        } else {
+            return <Text>Du är inte inloggad</Text>;
+        }
+    };
+
     render() {
+        if (this.props.data && this.props.data.video.length !== 0) {
+            console.log(this.props.data);
+        }
         const { navigate } = this.props.navigation;
         return (
             <ViewContainer>
                 <ScrollViewContainer>
-                    <View>
-                        <View style={[MainStyles.VIDEO_BUTTON]}>
-                            <Image
-                                style={MainStyles.VIDEO_CONTAINER}
-                                source={require('../../assets/icons/football.png')}
-                            />
-                            <TouchableHighlight
-                                underlayColor={StyleRules.BLUE_GRADIENT_COLOR}
-                                activeOpacity={0.8}
-                                style={styles.OPEN_VIDEO_BUTTON}
-                                onPress={() =>
-                                    navigate('VideoScreen', {
-                                        videoName: 'Landala 3',
-                                        videoDescription:
-                                            'I give a damn because a good',
-                                        videoStatus: 'not avalable',
-                                        videoType: 'avalable, live, ',
-                                        vidioUrl: 'url to video'
-                                    })}
-                            >
-                                <Text style={styles.BUTTON_TITLE}>
-                                    Lås upp video
-                                </Text>
-                            </TouchableHighlight>
-                        </View>
-
-                        <View style={[MainStyles.MAIN_CARD]}>
-                            <Text style={MainStyles.MAIN_CARD_TITLE}>
-                                SM i rugby
-                            </Text>
-                            <Text>
-                                Behind you, stands a symbol of oppression.
-                                Blackgate Prison, where a thousand men have
-                                languished under the name of this man: Harvey
-                                Dent.
-                            </Text>
-                        </View>
-                    </View>
-                    {/* <TouchableOpacity
-                        onPress={() =>
-                            navigate('VideoScreen', {
-                                videoName: 'SM final innebany',
-                                videoDescription:
-                                    'I give a damn because a good',
-                                videoStatus: 'live',
-                                videoType: 'live, ',
-                                vidioUrl: 'url to video'
-                            })}
-                    >
-                        <LiveNowCard videoName="SM final innebany" />
-                    </TouchableOpacity> */}
+                    {this.renderLiveSports()}
+                    {this.renderComponents()}
                 </ScrollViewContainer>
             </ViewContainer>
         );
@@ -98,3 +140,9 @@ const styles = StyleSheet.create({
         fontSize: 18
     }
 });
+
+function mapStateToProps(state) {
+    return { data: state.auth.data, authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(HomeScreen);

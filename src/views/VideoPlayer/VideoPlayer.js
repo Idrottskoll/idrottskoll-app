91332ab947f49eb26117165b93c77badb3d47a72
Jeremy Danenr <jeremydanner2@gmail.com>
2017-11-13 10:23:00 +0100

    removed buttons from videoplayer

--- src/views/VideoPlayer/VideoPlayer.js
+++ src/views/VideoPlayer/VideoPlayer.js
@@ -5,9 +5,8 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Video from 'react-native-video';
 import { VIDEO_URL } from '../../actions/config';
 
-// TODO: all binde methods in constructor make a callback function (looks better)
-// TODO: Fix styles and buttons
-// TODO: Add app colors to the styles
+// TODO: Add close button to navigate back
+// TODO: add paus button
 
 export default class VideoPlayer extends React.Component {
     constructor(props) {
@@ -15,7 +14,8 @@ export default class VideoPlayer extends React.Component {
     }
 
     static navigationOptions = ({ navigation }) => ({
-        header: false
+        header: false,
+        tabBarVisible: false
     });
 
     state = {
@@ -29,7 +29,8 @@ export default class VideoPlayer extends React.Component {
         paused: false,
         skin: 'custom',
         ignoreSilentSwitch: null,
-        isBuffering: false
+        isBuffering: false,
+        showControlls: false
     };
 
     onLoad = data => {
@@ -55,155 +56,6 @@ export default class VideoPlayer extends React.Component {
         }
     }
 
-    renderSkinControl(skin) {
-        const isSelected = this.state.skin == skin;
-        const selectControls = skin == 'native' || skin == 'embed';
-        return (
-            <TouchableOpacity
-                onPress={() => {
-                    this.setState({
-                        controls: selectControls,
-                        skin: skin
-                    });
-                }}
-            >
-                <Text
-                    style={[
-                        styles.controlOption,
-                        { fontWeight: isSelected ? 'bold' : 'normal' }
-                    ]}
-                >
-                    {skin}
-                </Text>
-            </TouchableOpacity>
-        );
-    }
-
-    renderResizeModeControl(resizeMode) {
-        const isSelected = this.state.resizeMode == resizeMode;
-
-        return (
-            <TouchableOpacity
-                onPress={() => {
-                    this.setState({ resizeMode: resizeMode });
-                }}
-            >
-                <Text
-                    style={[
-                        styles.controlOption,
-                        { fontWeight: isSelected ? 'bold' : 'normal' }
-                    ]}
-                >
-                    {resizeMode}
-                </Text>
-            </TouchableOpacity>
-        );
-    }
-
-    renderIgnoreSilentSwitchControl(ignoreSilentSwitch) {
-        const isSelected = this.state.ignoreSilentSwitch == ignoreSilentSwitch;
-
-        return (
-            <TouchableOpacity
-                onPress={() => {
-                    this.setState({ ignoreSilentSwitch: ignoreSilentSwitch });
-                }}
-            >
-                <Text
-                    style={[
-                        styles.controlOption,
-                        { fontWeight: isSelected ? 'bold' : 'normal' }
-                    ]}
-                >
-                    {ignoreSilentSwitch}
-                </Text>
-            </TouchableOpacity>
-        );
-    }
-
-    renderCustomSkin() {
-        const flexCompleted = this.getCurrentTimePercentage() * 100;
-        const flexRemaining = (1 - this.getCurrentTimePercentage()) * 100;
-        const { params } = this.props.navigation.state;
-        return (
-            <View style={styles.container}>
-                <TouchableOpacity
-                    style={styles.fullScreen}
-                    onPress={() => {
-                        this.setState({ paused: !this.state.paused });
-                    }}
-                >
-                    {params.liveURL ? (
-                        <Video
-                            source={{ uri: params.liveURL }}
-                            style={styles.fullScreen}
-                            rate={this.state.rate}
-                            paused={this.state.paused}
-                            volume={this.state.volume}
-                            muted={this.state.muted}
-                            ignoreSilentSwitch={this.state.ignoreSilentSwitch}
-                            resizeMode={this.state.resizeMode}
-                            onLoad={this.onLoad}
-                            onBuffer={this.onBuffer}
-                            onProgress={this.onProgress}
-                            repeat={false}
-                        />
-                    ) : (
-                        <Video
-                            source={{ uri: `${VIDEO_URL}/${params.videoUrl}` }}
-                            style={styles.fullScreen}
-                            rate={this.state.rate}
-                            paused={this.state.paused}
-                            volume={this.state.volume}
-                            muted={this.state.muted}
-                            ignoreSilentSwitch={this.state.ignoreSilentSwitch}
-                            resizeMode={this.state.resizeMode}
-                            onLoad={this.onLoad}
-                            onBuffer={this.onBuffer}
-                            onProgress={this.onProgress}
-                            repeat={false}
-                        />
-                    )}
-                </TouchableOpacity>
-
-                <View style={styles.controls}>
-                    <View style={styles.generalControls}>
-                        <View style={styles.skinControl}>
-                            {this.renderSkinControl('custom')}
-                            {this.renderSkinControl('native')}
-                            {this.renderSkinControl('embed')}
-                        </View>
-                    </View>
-
-                    <View style={styles.generalControls}>
-                        <View style={styles.resizeModeControl}>
-                            {this.renderResizeModeControl('cover')}
-                            {this.renderResizeModeControl('contain')}
-                            {this.renderResizeModeControl('stretch')}
-                        </View>
-                    </View>
-
-                    <View style={styles.trackingControls}>
-                        <View style={styles.progress}>
-                            <View
-                                style={[
-                                    styles.innerProgressCompleted,
-                                    { flex: flexCompleted }
-                                ]}
-                            />
-                            <View
-                                style={[
-                                    styles.innerProgressRemaining,
-                                    { flex: flexRemaining }
-                                ]}
-                            />
-                        </View>
-                    </View>
-                </View>
-            </View>
-        );
-    }
-
     renderNativeSkin() {
         const videoStyle =
             this.state.skin == 'embed'
@@ -212,6 +64,13 @@ export default class VideoPlayer extends React.Component {
         const { params } = this.props.navigation.state;
         return (
             <View style={styles.container}>
+                {/* <TouchableOpacity
+                    style={styles.fullScreen}
+                    onPress={() =>
+                        this.setState({
+                            ShowControlls: !this.state.ShowControlls
+                        })}
+                > */}
                 <View style={styles.fullScreen}>
                     {params.liveURL ? (
                         <Video
@@ -231,7 +90,9 @@ export default class VideoPlayer extends React.Component {
                         />
                     ) : (
                         <Video
-                            source={{ uri: `${VIDEO_URL}/${params.videoUrl}` }}
+                            source={{
+                                uri: `${VIDEO_URL}/${params.videoUrl}`
+                            }}
                             style={videoStyle}
                             rate={this.state.rate}
                             paused={this.state.paused}
@@ -247,31 +108,14 @@ export default class VideoPlayer extends React.Component {
                         />
                     )}
                 </View>
-                <View style={styles.controls}>
-                    <View style={styles.generalControls}>
-                        <View style={styles.skinControl}>
-                            {this.renderSkinControl('custom')}
-                            {this.renderSkinControl('native')}
-                            {this.renderSkinControl('embed')}
-                        </View>
-                    </View>
-
-                    <View style={styles.generalControls}>
-                        <View style={styles.resizeModeControl}>
-                            {this.renderResizeModeControl('cover')}
-                            {this.renderResizeModeControl('contain')}
-                        </View>
-                    </View>
-                </View>
+                {/* </TouchableOpacity> */}
             </View>
         );
     }
 
     render() {
         const { params } = this.props.navigation.state;
-        return this.state.controls
-            ? this.renderNativeSkin()
-            : this.renderCustomSkin();
+        return this.renderNativeSkin();
     }
 }
 
@@ -282,6 +126,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'black'
     },
+
     fullScreen: {
         position: 'absolute',
         top: 0,
@@ -289,6 +134,7 @@ const styles = StyleSheet.create({
         bottom: 0,
         right: 0
     },
+
     controls: {
         backgroundColor: 'transparent',
         borderRadius: 5,
@@ -297,52 +143,24 @@ const styles = StyleSheet.create({
         left: 4,
         right: 4
     },
+
     progress: {
         flex: 1,
         flexDirection: 'row',
         borderRadius: 3,
         overflow: 'hidden'
     },
+
     innerProgressCompleted: {
         height: 20,
         backgroundColor: '#cccccc'
     },
+
     innerProgressRemaining: {
         height: 20,
         backgroundColor: '#2C2C2C'
     },
-    generalControls: {
-        flex: 1,
-        flexDirection: 'row',
-        overflow: 'hidden',
-        paddingBottom: 10
-    },
-    skinControl: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center'
-    },
-    resizeModeControl: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    ignoreSilentSwitchControl: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    controlOption: {
-        alignSelf: 'center',
-        fontSize: 11,
-        color: 'white',
-        paddingLeft: 2,
-        paddingRight: 2,
-        lineHeight: 12,
-        fontFamily: 'Fjalla One'
-    },
+
     nativeVideoControls: {
         top: 184,
         height: 300
